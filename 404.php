48c9a0408a79168c41c11b471cd39a6a4a19c9af
hemingtsai <rarmicrozip@outlook.com>
2025-08-12 20:40:28 +0800

    添加404页面

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+
+<div id="main" role="main">
+    <h1 style="width:100%;text-align:center;font-size:28vh">404</h1>
+    <h3 style="width:100%;text-align:center">您访问的页面不存在</h3>
+</div>
+
+<?php $this->need('footer.php'); ?>
